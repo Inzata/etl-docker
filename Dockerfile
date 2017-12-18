b1fb7ed13d5fd613498617c1e30619266e1c16dc
Ales Cervenka <ales.cervenka@inzata.com>
2017-12-18 12:28:01 +0100

    FILE: Basing the container on Centos 7.

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
-FROM centos:6.7
+FROM centos:7
 MAINTAINER Ales Cervenka <ales.cervenka@inzata.com>
 
 RUN yum install -y yum-plugin-ovl
 
 # Inzata ETL
 
+RUN yum install -y epel-release && yum clean all
+
 RUN yum install -y yum-utils wget && \
-	wget https://repo.inzata.com/etl/el6/inzataetl-5.x.repo -O/etc/yum.repos.d/inzataetl-5.x.repo && \
+	wget https://repo.inzata.com/etl/el7/inzataetl-5.x.repo -O/etc/yum.repos.d/inzataetl-5.x.repo && \
 	yum clean all && \
 	yum install -y inzata-etlp
 
@@ -14,7 +16,11 @@ ADD bootstrap.sh /etc/bootstrap.sh
 RUN chown root:root /etc/bootstrap.sh && \
 	chmod 700 /etc/bootstrap.sh
 
-CMD ["/etc/bootstrap.sh", "-d"]
+VOLUME /var/etl
+
+ENTRYPOINT ["/etc/bootstrap.sh"]
+
+CMD ["-d"]
 
 EXPOSE 80
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,58 @@
 #!/bin/bash
 
-service httpd start
+for directory in /var/etl/etc/httpd/conf.d /var/etl/www/html /var/etl/projects;
+do
+	if ! [ -d $directory ]; then
+		mkdir -p $directory
+		if ! [ -d $directory ]; then
+			echo "ERROR: Failed to create the $directory"
+			exit 2
+		fi
+	fi
+done
 
-if [[ $1 == "-d" ]]; then
-  while true; do sleep 1000; done
+# httpd
+
+if ! [ -L /etc/httpd/conf.d ]; then
+	if [ -e /etc/httpd/conf.d ]; then
+		cp -f /etc/httpd/conf.d/* /var/etl/etc/httpd/conf.d/ && rm -fr /etc/httpd/conf.d/;
+	fi
+	cd /etc/httpd/
+	ln -s /var/etl/etc/httpd/conf.d conf.d
+
+	if ! [ -L /etc/httpd/conf.d ]; then
+		echo "ERROR: Failed to create a link /etc/httpd/conf.d";
+		exit 2;
+	fi
 fi
 
-if [[ $1 == "-bash" ]]; then
-  /bin/bash
+# html
+
+if ! [ -L /var/www/html ]; then
+	if [ -e /var/www/html ]; then
+		cp -fr /var/www/html /var/etl/www/ && rm -fr /var/www/html/;
+	fi
+	cd /var/www
+	ln -s /var/etl/www/html html
+
+	if ! [ -L /var/www/html ]; then
+		echo "ERROR: Failed to create a link /var/etl/www/html/";
+		exit 2;
+	fi
 fi
+
+# projects
+
+if ! [ -L /var/data/etl ]; then
+	if [ -e /var/data/etl ]; then
+		echo "You need to move the data from /var/data/etl to /var/etl/projects manually"
+		exit 2
+	fi
+	mkdir -p /var/data
+	cd /var/data
+	ln -s /var/etl/projects etl
+fi
+
+apachectl -k start
+
+while true; do sleep 1000; done
